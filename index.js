0a3b4575833cd7adb9b3f8436c2a8b63e43e9d4a
AJ ONeal <coolaj86@gmail.com>
2015-12-30 02:47:38 +0000

    get all external ips

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var PromiseA = require('bluebird');
-var requestAsync = PromiseA.promisify(require('request'));
 
 module.exports.create = function (args) {
   var promises = [];
@@ -14,17 +13,16 @@ module.exports.create = function (args) {
   // TODO determine if we have a AAAA local ip or not
   // TODO get A and AAAA records
   if (-1 !== args.protocols.indexOf('none')) {
-    promises.push(PromiseA.some(args.ipifyUrls.map(function (ipifyUrl) {
-      return requestAsync('https://' + ipifyUrl).then(function (resp) {
-        var ip = (resp.body || '').toString('ascii');
+    promises.push(PromiseA.any(args.ipifyUrls.map(function (ipifyUrl) {
+      var getIp = require('./lib/external-ip');
 
-        if (!/\d+\.\d+\.\d+\.\d+/.test(ip) && !/\w+\:\w+/.test(ip)) {
-          return PromiseA.reject(new Error("bad response '" + resp.body + "'"));
-        }
-
+      return getIp({ hostname: ipifyUrl, debug: args.debug });
+    })).then(function (ips) {
+      return ips.map(function (ip) {
+        // TODO attempt loopback
         return ip;
       });
-    }), 1));
+    }));
   }
 
   return PromiseA.all(promises).then(function (results) {
