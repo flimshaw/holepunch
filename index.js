6ecb868ae1ce4064bfee80a10a4235bed41df001
AJ ONeal <coolaj86@gmail.com>
2015-12-30 03:36:14 +0000

    test loopback listener

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
 'use strict';
 
-var PromiseA = require('bluebird');
-
-module.exports.create = function (args) {
-  var promises = [];
-
-  if (args.debug) {
-    console.log('[HP] create holepuncher');
-    console.log(args);
-  }
-
-  // TODO determine if we have a AAAA local ip or not
-  // TODO get A and AAAA records
-  if (-1 !== args.protocols.indexOf('none')) {
-    promises.push(PromiseA.any(args.ipifyUrls.map(function (ipifyUrl) {
-      var getIp = require('./lib/external-ip');
-
-      return getIp({ hostname: ipifyUrl, debug: args.debug });
-    })).then(function (ips) {
-      return ips.map(function (ip) {
-        // TODO attempt loopback
-        return ip;
-      });
-    }));
-  }
-
-  return PromiseA.all(promises).then(function (results) {
-    if (args.debug) {
-      console.log('[HP] all done');
-      console.log(results);
-    }
-  });
-};
+module.exports = require('./lib/index.js');
